2a86511e5a1a66b1afae3502fe63a2972e52e850
Duc <wookie99@tpg.com.au>
2022-08-15 19:17:47 +0800

    CSS Javascript and Images

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create A Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "My super secret key that no one is supposed to know"
+
+# Create A Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # Create a route decorator
 @app.route('/')
@@ -19,11 +27,9 @@ app = Flask(__name__)
 # trim
 # striptags
 
-
 def index():
     first_name = "John"
     stuff = "This is bold Text"
-
     favorite_pizza = ["Pepperoni", "Cheese", "Mushrooms", 41]
     return render_template("index.html", 
     first_name=first_name,
@@ -48,3 +54,17 @@ def page_not_found(e):
 def page_not_found(e):
         return render_template("500.html"), 500
 
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
+    return render_template("name.html",
+        name = name,
+        form = form)
